c99b613219e27785c5ed40926958cf8041542235
lee_c <gitleelee@gmail.com>
2023-12-28 13:21:34 -0800

    Wrote Stateful Components to handle user input.

---
+++ src/UserInput.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class UserInput extends Component {
+  state = {
+    name: "",
+  };
+
+  updateName = (e) => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addName(this.state.name);
+    this.setState({ name: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {console.log(this.props)}
+        {/* {console.log(this.state)} */}
+        <input
+          type="text"
+          placeholder="Add a new name here..."
+          value={this.state.name}
+          onChange={this.updateName}
+        />
+        <input type="submit" value="Create Name Tag" />
+      </form>
+    );
+  }
+}
+
+export default UserInput;
