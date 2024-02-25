a77c7571b092c09a4935c364e212d96f95ef8726
lee_c <gitleelee@gmail.com>
2024-02-24 23:50:41 -0800

    Added comments.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+// Importing necessary React components
 import React, { Component } from "react";
 import NameTagList from "./NameTagList.js";
 import UserInput from "./UserInput.js";
 
+// App component definition
 class App extends Component {
+  // Initial state with an empty array for names
   state = {
     names: [],
   };
+
+  // Function to remove a name from the list based on its index
   removeName = (clickedIndex) => {
     // to learn how the .filter method works, check out https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
     const filterCallback = (_, index) => index !== clickedIndex;
@@ -13,16 +18,20 @@ class App extends Component {
     this.setState({ names: newNames });
   };
 
+  // Function to add a new name to the list
   addName = (name) => {
+    // Creating a new array with the new name at the beginning
     const newNames = [name, ...this.state.names];
     this.setState({ names: newNames });
   };
 
+  // Lifecycle method: Save names data to local storage after each updat
   componentDidUpdate() {
     const savedNameString = JSON.stringify(this.state.names);
     localStorage.setItem("savedNames", savedNameString);
   }
 
+  // Lifecycle method: Read saved names data from local storage after first render
   componentDidMount() {
     const savedNamesStrings = localStorage.getItem("savedNames");
     if (savedNamesStrings) {
@@ -31,15 +40,19 @@ class App extends Component {
     }
   }
 
+  // Render method: Output the Name Tag Generator UI components
   render() {
     return (
       <div className="App">
         <h1>Name Tag Generator</h1>
+        {/* UserInput component for adding names */}
         <UserInput addName={this.addName} />
+        {/* NameTagList component for displaying and removing names */}
         <NameTagList names={this.state.names} removeName={this.removeName} />
       </div>
     );
   }
 }
 
+// Exporting the App component as the default export
 export default App;
